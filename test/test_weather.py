079b7e2d330d5654a82781add77240268294e01c
SlavaTatanov <slavatatanov@gmail.com>
2023-07-26 21:17:28 +0500

    weekend test

--- test/test_weather.py
+++ test/test_weather.py
@@ -7,10 +7,11 @@ from Bot.CALLBACKS import CURRENT
 class TestWeather(unittest.TestCase):
     def setUp(self) -> None:
         self.obj = Weather((55.30, 61.22), date(2023, 7, 5), CURRENT)
+        self.obj2 = Weather((55.30, 61.22), date(2023, 7, 23), CURRENT)
 
     def test_weekend_days(self):
         self.assertEqual(self.obj._weekend_days(),
-                          (date(2023, 7, 8), date(2023, 7, 9)),
-                          "Вычисление выходных дней")
-
-
+                         (date(2023, 7, 8), date(2023, 7, 9)),
+                         "Вычисление выходных дней")
+        self.assertEqual(self.obj2._weekend_days(), (date(2023, 7, 29), date(2023, 7, 30)),
+                         "Запрос выходных дней в воскресенье")
